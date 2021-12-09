8e3f9132842dc38f454e25907cbe4a539a224331
lishu <lishugupta652@gmail.com>
2021-12-09 21:32:34 +0530

    register route working

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,23 +10,23 @@ router.get("/", (req, res) => {
 // REGISTER
 router.post("/register", async (req, res) => {
   console.log(req.body);
-  //   // Validating the data
-  //   const { error, data } = registerValidation(req.body);
-  //   if (error) return res.status(400).send(error.details[0].message);
-  //   console.log(error, data);
+  // Validating the data
+  const { error, data } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  console.log(error, data);
 
-  //   const userUser = new User({
-  //     username: req.body.username,
-  //     email: req.body.email,
-  //     password: req.body.password,
-  //   });
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-  //   try {
-  //     const user = await newUser.save();
-  //     res.status(200).send(user);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
+  try {
+    const user = await newUser.save();
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(400).send("Error in database");
+  }
 });
 
 module.exports = router;
