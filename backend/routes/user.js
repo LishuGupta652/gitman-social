5ac372ba473c14a53bcaae8c91b14f24880adafa
lishu <lishugupta652@gmail.com>
2021-12-10 01:14:47 +0530

    update user route /:id

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -36,6 +36,33 @@ router.put("/:id", async (req, res) => {
   }
 });
 // delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    // Check if user wanna update his password
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json({ error: err });
+      }
+    }
+
+    // Details other than password
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json({ success: "Account has been updated" });
+    } catch (err) {
+      return res
+        .status(403)
+        .json({ error: "you can delete only your account" });
+    }
+  } else {
+    return res.status(403).json({ error: "you can delete only your account" });
+  }
+});
 // get a user
 // follow user
 // unfollow user
