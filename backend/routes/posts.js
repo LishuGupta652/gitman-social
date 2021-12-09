b25127be6c66b3949fa0ad8495911ba3ca27d2d9
lishu <lishugupta652@gmail.com>
2021-12-10 02:09:55 +0530

    route

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  console.log("Posts route");
+});
+
+// Create a post
+// Get a post
+// Update a post
+// Delete a post
+
 module.exports = router;
