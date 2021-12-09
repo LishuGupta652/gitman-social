88e07c7bb694e9f02da6535c40442d6b68fff99b
lishu <lishugupta652@gmail.com>
2021-12-10 02:13:10 +0530

    postData:

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,24 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
 router.get("/", (req, res) => {
   console.log("Posts route");
 });
 
 // Create a post
-// Get a post
+router.post("/", async (req, res) => {
+  const post = new Post({});
+  try {
+    const savedPost = post.save();
+    res.status(200).json({ success: true, post: savedPost });
+  } catch (error) {
+    res.status(500).send({ error: "server Error accured" });
+  }
+});
 // Update a post
 // Delete a post
+// Like a post
+// Get a post
+// Get timeline posts
 
 module.exports = router;
