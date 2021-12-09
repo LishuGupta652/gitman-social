1d05bd29b8acd763489cc1d61654fb8dcc22f3c0
lishu <lishugupta652@gmail.com>
2021-12-10 01:14:10 +0530

    update user route /:id

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,40 @@
 const router = require("express").Router();
 const { registerValidation, loginValidation } = require("../validate");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 router.get("/", (req, res) => {
   res.send("inside user route");
 });
 
 // update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    // Check if user wanna update his password
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json({ error: err });
+      }
+    }
+
+    // Details other than password
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json({ success: "Account has been updated" });
+    } catch (err) {
+      return res
+        .status(403)
+        .json({ error: "you can update only your account" });
+    }
+  } else {
+    return res.status(403).json({ error: "you can update only your account" });
+  }
+});
 // delete user
 // get a user
 // follow user
