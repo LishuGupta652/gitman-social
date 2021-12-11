11cbf959f0b1967f2bee13ec6920fb9b0471f810
lishu <lishugupta652@gmail.com>
2021-12-11 22:36:26 +0530

    tpobar

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
 import PersonIcon from "@mui/icons-material/Person";
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { Feed } from "@mui/icons-material";
+import { styled } from "styled-components";
+
+const HomeContainer = styled.div``;
 
 const Home = () => {
   return (
     <div>
       <Topbar />
+      <HomeContainer>
+        <Sidebar />
+        <Feed />
+      </HomeContainer>
     </div>
   );
 };
