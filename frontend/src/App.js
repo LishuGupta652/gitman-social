d8d232f36dc8e9b892c8de93f39e2d820cfe3eef
lishu <lishugupta652@gmail.com>
2021-12-11 17:34:24 +0530

    front end started

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container-wrapper">
+          <div className="container">Hello world</div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
