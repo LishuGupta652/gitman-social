f7900977fc36f68bafcb444ccc56387e8146d627
lishu <lishugupta652@gmail.com>
2021-12-11 22:40:22 +0530

    tpobar

--- frontend/src/components/topbar/Topbar.js
+++ frontend/src/components/topbar/Topbar.js
@@ -1,10 +1,99 @@
-import "./topbar.css";
 import { Chat, Notifications, Search } from "@mui/icons-material/";
 import Person from "@mui/icons-material/Person";
+import styled from "styled-components";
+
+const TopbarContainer = styled.div`
+  height: 50px;
+  width: 100%;
+  background-color: #1277f2;
+  display: flex;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  .topbarLeft {
+    flex: 3;
+  }
+
+  .topbarCenter {
+    flex: 5;
+  }
+
+  .topbarRight {
+    flex: 4;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    color: white;
+  }
+  .topbarLink {
+    margin-right: 10px;
+    font-style: 13px;
+    cursor: pointer;
+  }
+  .topbarIcons {
+    display: flex;
+  }
+  .topbarIconItem {
+    margin-right: 15px;
+    cursor: pointer;
+    position: relative;
+  }
+  .topbarIconBadge {
+    width: 15px;
+    height: 15px;
+    background-color: red;
+    color: white;
+
+    position: absolute;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 12px;
+    top: -5px;
+    right: -5px;
+  }
+
+  .topbarImg {
+    width: 32px;
+    height: 32px;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+  .logo {
+    font-size: 24px;
+    margin-left: 20px;
+    font-weight: bold;
+    color: white;
+    cursor: pointer;
+  }
+
+  .searchbar {
+    width: 100%;
+    height: 30px;
+    background-color: white;
+    border-radius: 30px;
+    display: flex;
+    align-items: center;
+    outline: none;
+  }
+  .seachIcon {
+    font-size: 20px !important;
+    margin-left: 10px;
+  }
+
+  .searchInput {
+    border: none;
+    width: 70%;
+  }
+  .searchInput:focus {
+    outline: none;
+  }
+`;
 
 const Topbar = () => {
   return (
-    <div className="topbarContainer">
+    <TopbarContainer>
       <div className="topbarLeft">
         <span className="logo">GitmanSocial</span>
       </div>
@@ -38,7 +127,7 @@ const Topbar = () => {
         </div>
         <img src="/lishu.jpg" alt="lishugupta" className="topbarImg" />
       </div>
-    </div>
+    </TopbarContainer>
   );
 };
 
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -3,7 +3,8 @@ import PersonIcon from "@mui/icons-material/Person";
 import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { Feed } from "@mui/icons-material";
-import { styled } from "styled-components";
+import styled from "styled-components";
+import Rightbar from "../../components/rightbar/Rightbar";
 
 const HomeContainer = styled.div``;
 
@@ -14,6 +15,7 @@ const Home = () => {
       <HomeContainer>
         <Sidebar />
         <Feed />
+        <Rightbar />
       </HomeContainer>
     </div>
   );
