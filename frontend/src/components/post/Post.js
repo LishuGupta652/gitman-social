d9d2b548de62a6358f9ac8d8432875dd2c424a46
lishu <lishugupta652@gmail.com>
2021-12-14 15:29:14 +0530

    post component

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -6,7 +6,14 @@ const Post = () => {
   return (
     <PostStyled>
       <div className="wrapper">
-        <div className="top"></div>
+        <div className="top">
+          <div className="topLeft">
+            <img src="/lishu.jpg" className="postProfileImage" alt="" />
+            <span className="postUsername">Lishu Gupta</span>
+            <span className="postDate">4 min ago</span>
+          </div>
+          <div className="topRight"></div>
+        </div>
         <div className="center"></div>
         <div className="bottom"></div>
       </div>
