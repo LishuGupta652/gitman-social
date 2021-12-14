06992724aea228fde4d17f64fe91cca07ae6c084
lishu <lishugupta652@gmail.com>
2021-12-14 15:15:03 +0530

    share componnent

--- frontend/src/components/share/Share.js
+++ frontend/src/components/share/Share.js
@@ -6,7 +6,7 @@ const ShareStyled = styled.div`
   width: 100%;
   height: 170px;
   border-radius: 10px;
-  background-color: #ccc;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   wrapper {
   }
 `;
